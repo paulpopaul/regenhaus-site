26c798d4e2a7f7d0c9f3bd30433cf315f59195a8
Diego Ulloa <diegoulloao@icloud.com>
2019-07-10 14:45:59 -0400

    Nuevos ajutes estilos servicios / bd: desarrollado por

--- wordpress/wp-content/themes/regenhaus-template/js/custom.js
+++ wordpress/wp-content/themes/regenhaus-template/js/custom.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
             var $this = jQuery($this)
             var pointer = jQuery(window).scrollTop() + jQuery(window).height() / 2
 
-            if ( pointer >= $this.offset().top - 40 && pointer <= $this.offset().top + $this.height() + 40 ) {
+            if ( pointer >= $this.offset().top - 60 && pointer <= $this.offset().top + $this.height() + 60 ) {
                 $this.addClass("hover")
 
             } else {
